d9d2286117dfd36028d793bac5dae15df9e6bed7
Adiaksa31 <programerbiasa.com>
2022-09-28 15:57:48 +0800

    menambahkan beberapa fitur di home (list, bantuan)

--- lib/home.dart
+++ lib/home.dart
@@ -144,20 +144,369 @@ class home extends StatelessWidget {
                       ],
                     )),
                 Container(
-                  height: 49,
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: double.infinity,
+                  // color: Colors.white,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(255, 204, 202, 202),
+                        // spreadRadius: 5,
+                        offset: Offset(1, 3), // changes position of shadow
+                      ),
+                    ],
+                    border: Border.all(
+                        color: const Color(0xFF000000),
+                        style: BorderStyle.solid), //Border.all
+                    /*** The BorderRadius widget  is here ***/
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: Column(children: [
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(padding: EdgeInsets.all(5)),
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Icon(Icons.wallet,
+                                      size: 35,
+                                      color: Color.fromARGB(255, 0, 0, 0)),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                ),
+                                Container(
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => home()));
+                                    },
+                                    child: Text(
+                                      "Cek Saldo",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(255, 7, 7, 7),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(10)),
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Icon(Icons.monetization_on,
+                                      size: 35,
+                                      color: Color.fromARGB(255, 0, 0, 0)),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                ),
+                                Container(
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => home()));
+                                    },
+                                    child: Text(
+                                      "Transfer",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(255, 7, 7, 7),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(10)),
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Icon(Icons.money,
+                                      size: 35,
+                                      color: Color.fromARGB(255, 0, 0, 0)),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                ),
+                                Container(
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => home()));
+                                    },
+                                    child: Text(
+                                      "Deposit",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(255, 7, 7, 7),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(5)),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(padding: EdgeInsets.all(5)),
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Icon(Icons.credit_card,
+                                      size: 35,
+                                      color: Color.fromARGB(255, 0, 0, 0)),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                ),
+                                Container(
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => home()));
+                                    },
+                                    child: Text(
+                                      "Pembayaran",
+                                      style: TextStyle(
+                                        fontSize: 9,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(255, 7, 7, 7),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(10)),
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Icon(Icons.attach_money,
+                                      size: 35,
+                                      color: Color.fromARGB(255, 0, 0, 0)),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                ),
+                                Container(
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => home()));
+                                    },
+                                    child: Text(
+                                      "Pinjaman",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(255, 7, 7, 7),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(10)),
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Icon(Icons.insert_chart,
+                                      size: 35,
+                                      color: Color.fromARGB(255, 0, 0, 0)),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                ),
+                                Container(
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => home()));
+                                    },
+                                    child: Text(
+                                      "mutasi",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(255, 7, 7, 7),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(5)),
+                        ],
+                      ),
+                    ),
+                  ]),
+                ),
+                Container(
+                  height: 20,
                 ),
                 Container(
                     color: Color.fromARGB(255, 92, 115, 122),
                     width: 600,
-                    height: 40,
-                    child: Center(
-                        child: Text(
-                      "copyright @2022 by Undiksha",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 10,
-                          fontWeight: FontWeight.w600),
-                    ))),
+                    height: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 200,
+                                padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                                child: Text(
+                                  "Butuh Bantuan?",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                child: Text(
+                                  "0878-1234-1024",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Icon(Icons.phone,
+                              size: 50, color: Color.fromARGB(255, 0, 0, 0)),
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        ),
+                      ],
+                    )),
               ],
             ),
           ),
